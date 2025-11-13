089cfe2bb2c34f7b9e247e5e6c483e101da00d33
landonk89 <landonkkeith@gmail.com>
2025-11-13 12:36:19 -0500

    Add hud crosshair, accurately shows where a bullet will hit in the world, shows a red indicator when aimed at a person.

--- Components/HudCrosshair.cs
+++ Components/HudCrosshair.cs
@@ -1,5 +1,4 @@
-﻿using BepInEx.Logging;
-using EFT;
+﻿using EFT;
 using HEVSuitMod.Types;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,25 +7,29 @@ namespace HEVSuitMod.Components;
 
 public class HudCrosshair : MonoBehaviour
 {
-    private readonly ManualLogSource log = BepInEx.Logging.Logger.CreateLogSource($"{typeof(HudCrosshair).FullName}");
+    //private readonly ManualLogSource log = BepInEx.Logging.Logger.CreateLogSource($"{typeof(HudCrosshair).FullName}");
     private Player.FirearmController currentFirearm;
     private HudIcon crosshair;
     private HudIcon crosshairOnTarget;
+    private Camera camera;
+    private readonly int layerMask = LayerMaskClass.PlayerCollisionsMask + LayerMaskClass.HitColliderMask; //537136073
+
+    private HudController Hud => HEVMod.Instance.HudController;
 
 #pragma warning disable IDE0051
     private void Awake()
     {
         crosshair = new(transform.Find("Crosshair").GetComponent<Image>());
         crosshair.Image.enabled = false;
-        crosshairOnTarget = new(transform.Find("CrosshairOnTarget").GetComponent<Image>());
+        crosshairOnTarget = new(crosshair.Image.transform.Find("OnTarget").GetComponent<Image>());
         crosshairOnTarget.Image.enabled = false;
 
-        if (GamePlayerOwner.MyPlayer.HandsController is not Player.FirearmController faController)
-            log.LogWarning("Start() HandsController is not Player.FirearmController.");
-        else
-            currentFirearm = faController;
+        if (GamePlayerOwner.MyPlayer.HandsController is Player.FirearmController faController)
+            currentFirearm = faController; // Only set if it's a gun
 
         GamePlayerOwner.MyPlayer.HandsChangedEvent += HandsChanged;
+		currentFirearm?.OnAimingChanged += AimingChanged;
+		camera = CameraClass.Instance.Camera;
     }
 
     private void OnDestroy()
@@ -36,18 +39,55 @@ public class HudCrosshair : MonoBehaviour
 
     private void Update()
     {
+        // Handle crosshair states
+        switch (crosshair.State)
+        {
+            case EHudIconState.Activate:
+                if (Hud.UpdateTransition(crosshair, HudController.FADE_TIME, Color.white) && Hud.UpdateTransition(crosshairOnTarget, HudController.FADE_TIME, Color.white))
+                {
+                    crosshair.State = EHudIconState.Active;
+                    crosshairOnTarget.State = EHudIconState.Active;
+                }
+                break;
+
+            case EHudIconState.Deactivate:
+                if (Hud.UpdateTransition(crosshair, HudController.FADE_TIME, Color.clear) && Hud.UpdateTransition(crosshairOnTarget, HudController.FADE_TIME, Color.clear))
+                {
+                    crosshair.State = EHudIconState.Inactive;
+                    crosshairOnTarget.State = EHudIconState.Inactive;
+                }
+                break;
+        }
+
+        // TODO: Fade out if we're ADS, fade back in when unADS
         if (currentFirearm == null)
             return;
 
-        // TODO: Get muzzle direction, RayCast to hit point, convert to screen space, reposition crosshair.
-        // Also enable crosshairOnTarget if hit point is a living thing
+        // Our crosshair shows where the bullet will hit instead of being centered all the time
+        if (Physics.Raycast(currentFirearm.FireportPosition, currentFirearm.WeaponDirection, out RaycastHit hit, 1024f, layerMask))
+            crosshair.Image.rectTransform.position = camera.WorldToScreenPoint(hit.point);
+
+        // If we are aiming at a player/bot, activate the 'on target' indicator
+        if (hit.collider?.GetComponent<IPlayer>() != null)
+            crosshairOnTarget.Image.enabled = true;
+        else
+            crosshairOnTarget.Image.enabled = false;
     }
 #pragma warning restore IDE0051
 
+    private void AimingChanged(bool aiming)
+    {
+        EHudIconState newState = aiming ? EHudIconState.Deactivate : EHudIconState.Activate;
+        Hud.StartTransition(crosshair, newState);
+        Hud.StartTransition(crosshairOnTarget, newState);
+    }
+
     private void HandsChanged(IHandsController controller)
     {
+        currentFirearm.OnAimingChanged -= AimingChanged;
         if (controller is Player.FirearmController faController)
         {
+            faController.OnAimingChanged += AimingChanged;
             currentFirearm = faController;
             crosshair.Image.enabled = true;
         }
