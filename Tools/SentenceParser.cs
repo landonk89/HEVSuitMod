03ebfeeab94675414f9a0645bc2dd250094af238
landonk89 <landonkkeith@gmail.com>
2025-11-29 14:01:37 -0500

    Optimized sentence parser using ReadOnlySpan in place of strings

--- Tools/SentenceParser.cs
+++ Tools/SentenceParser.cs
@@ -2,6 +2,8 @@ using BepInEx.Logging;
 using HEVSuitMod.Types;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Security;
 using UnityEngine;
 
 namespace HEVSuitMod.Tools;
@@ -30,6 +32,9 @@ public class SentenceParser
 
 	public void ParseAllSentences()
 	{
+		Stopwatch timetaken = new();
+		timetaken.Start();
+
 		TextAsset hevSentencesFile = Assets.LoadAsset<TextAsset>(SENTENCES_FILE);
 		if (hevSentencesFile == null)
 		{
@@ -59,58 +64,89 @@ public class SentenceParser
 		log.LogInfo($"Parsed {sentenceCount} sentences.");
 		if (missingFiles.Count > 0)
 			log.LogWarning($"Encountered {missingFiles.Count} missing files:\n{BundleUtils.FileTree(missingFiles)}");
+
+		timetaken.Stop();
+		log.LogWarning($"Parser elapsed time: {timetaken.Elapsed}");
 	}
 
 	// --------------------------------------------------------------
 	// HEVSentence:
 	// The first word is the event name or itemId like 'Death' or '5926bb2186f7744b1c6c6e60'
-	// Then each sound filename is placed in line with tags before it enclosed in sqaure brackets [ ], each tag inside is separated by commas ','.
+	// Then each sound filename is placed in line with tags before it enclosed in square brackets [ ], each tag inside is separated by commas ','.
 	// Multiple tags per file are supported, so something like '[delay:0.5,loop:2,pitch:1.2,volume:0.8]filename' will work
 	// Example sentence: Death [loop:2]fx/beep [delay:0.1,loop:2]fx/beep [delay:0.1]fx/beep [delay:0.1]fx/beep [delay:0.1,pitch:1.2,volume:0.5]fx/flatline
 	// --------------------------------------------------------------
 	public HEVSentence ParseSentence(string sentence)
 	{
+		ReadOnlySpan<char> span = sentence.AsSpan();
 		List<HEVAudioClip> clips = [];
-		string[] tokens = sentence.Split(' ');
-		log.LogDebug($"ParseSentence: {sentence}");
 
-		// Parse tokenized sentence
-		for (int i = 1; i < tokens.Length; i++)
+		int index = span.IndexOf(' ');
+		var firstToken = span[..index];
+		int position = index + 1;
+		while (position <= span.Length)
 		{
-			string path;
-			AudioClip clip;
+			int next = span[position..].IndexOf(' ');
+			ReadOnlySpan<char> token;
+			if (next == -1)
+			{
+				token = span[position..];
+				position = span.Length + 1;
+			}
+			else
+			{
+				token = span.Slice(position, next);
+				position += next + 1;
+			}
+
+			if (token.IsEmpty)
+				continue;
+
 			int loops = 1;
 			float interval = 0f; // Default space between loops
 			float pitch = 1f;
 			float volume = HEVSuitMod.Instance.globalVolume.Value;
 			float delay = HEVSuitMod.DEFAULT_PLAYBACK_DELAY;
 
-			// For each token there may be parameters formatted like [param:value,param2:value]
-			if (tokens[i].StartsWith("["))
+			var file = token;
+			if (!token.IsEmpty && token[0] == '[')
 			{
-				string[] parameters = tokens[i].Substring(1, tokens[i].IndexOf(']') - 1).Split([','], StringSplitOptions.RemoveEmptyEntries);
-				for (int j = 0; j < parameters.Length; j++)
+				int end = token.IndexOf(']');
+				var parameters = token[1..end];
+				while (!parameters.IsEmpty)
 				{
-					string[] paramValuePair = parameters[j].Split(':');
-					string key = paramValuePair[0];
-					string val = paramValuePair[1];
-
-					switch (key)
+					int comma = parameters.IndexOf(',');
+					ReadOnlySpan<char> pair;
+					if (comma == -1)
 					{
-						case "l" when int.TryParse(val, out int lps): loops = lps; break;
-						case "i" when float.TryParse(val, out float intvl): interval = intvl; break;
-						case "p" when float.TryParse(val, out float pch): pitch = pch; break;
-						case "v" when float.TryParse(val, out float vol): volume *= vol; break;
-						case "d" when float.TryParse(val, out float dly): delay = dly; break;
+						pair = parameters;
+						parameters = [];
 					}
+					else
+					{
+						pair = parameters[..comma];
+						parameters = parameters[(comma + 1)..];
+					}
+
+					int colon = pair.IndexOf(':');
+					var key = pair[..colon];
+					var val = pair[(colon + 1)..];
+
+					if (key.SequenceEqual("l"))
+						loops = int.Parse(val);
+					else if (key.SequenceEqual("i"))
+						interval = float.Parse(val);
+					else if (key.SequenceEqual("p"))
+						pitch = float.Parse(val);
+					else if (key.SequenceEqual("v"))
+						volume *= float.Parse(val);
+					else if (key.SequenceEqual("d"))
+						delay = float.Parse(val);
 				}
-				path = workingDirectory + tokens[i].Substring(tokens[i].IndexOf(']') + 1).ToLower() + ".wav";
-			}
-			else // Token is just filename, no params
-			{
-				path = workingDirectory + tokens[i].ToLower() + ".wav";
+				file = token[(end + 1)..];
 			}
 
+			string path = (workingDirectory + file.ToString().ToLower() + ".wav");
 			if (!allFiles.Contains(path.ToLower()))
 			{
 				string missingFile = $"hevsuit.bundle/{path}";
@@ -119,10 +155,9 @@ public class SentenceParser
 
 				continue;
 			}
-			clip = Assets.LoadAsset<AudioClip>(path);
-			clips.Add(new HEVAudioClip(clip, loops, interval, pitch, volume, delay));
+			var clipAsset = Assets.LoadAsset<AudioClip>(path);
+			clips.Add(new HEVAudioClip(clipAsset, loops, interval, pitch, volume, delay));
 		}
-
-		return new HEVSentence(tokens[0], clips);
+		return new HEVSentence(firstToken.ToString(), clips);
 	}
 }
